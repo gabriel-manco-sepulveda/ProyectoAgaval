3ba8cad486bca0cf36ab7d5184b43443cf8fc39c
GabrielManco <gabomansep@gmail.com>
2023-06-25 16:40:54 -0500

    Configuracion del proyecto

---
+++ SistemaInventario.AccesoDatos/Migrations/20230625212413_AgregarBodegaMigration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.AccesoDatos.Migrations
+{
+    public partial class AgregarBodegaMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bodegas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bodegas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bodegas");
+        }
+    }
+}
---
+++ SistemaInventario.Modelos/Bodega.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace SistemaInventario.Modelos
+{
+    public class Bodega
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El nombre es requerido")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage ="La descripción es requerida")]
+        public string Descripcion { get; set; }
+        [Required(ErrorMessage ="El estado es requerido")]
+        public bool Estado { get; set; }
+    }
+}
