6a418d1b299873ff2c832075d947fc438ed915f5
GabrielManco <gabomansep@gmail.com>
2023-07-08 11:36:39 -0500

    CRUD Categorias

---
+++ SistemaInventario/Areas/Admin/Controllers/CategoriaController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SistemaInventario.AccesoDatos.Repositorio;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+using SistemaInventario.Modelos;
+using SistemaInventario.Utilidades;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriaController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+
+        public CategoriaController(IUnidadTrabajo unidadTrabajo)
+        {
+            _unidadTrabajo = unidadTrabajo;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Upsert(int? id)
+        {
+            Categoria categoria = new Categoria();
+            if (id == null)
+            {
+                //Crear una nueva Categoria
+                categoria.Estado = true;
+                return View(categoria);
+            }
+            //Actualizamos Categoria
+            categoria = await _unidadTrabajo.Categoria.Obtener(id.GetValueOrDefault());
+            if(categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                if (categoria.Id == 0)
+                {
+                    await _unidadTrabajo.Categoria.Agregar(categoria);
+                    TempData[DefinicionesEstaticas.Exitosa] = "Categoría creada exitosamente";
+                }
+                else
+                {
+                    _unidadTrabajo.Categoria.Actualizar(categoria);
+                    TempData[DefinicionesEstaticas.Exitosa] = "Categoría actualizada correctamente";
+                }
+                await _unidadTrabajo.Guardar();
+                return RedirectToAction(nameof(Index)); 
+            }
+            TempData[DefinicionesEstaticas.Error] = "Error al grabar Categoria";
+            return View(categoria);
+        }
+
+        #region API
+
+        [HttpGet]
+
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var todos = await _unidadTrabajo.Categoria.ObtenerTodos();
+            return Json(new { data = todos });
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var categoriaDb = await _unidadTrabajo.Categoria.Obtener(id);
+            if(categoriaDb == null)
+            {
+                return Json(new { success = false, mesagge = "Error al borrar la Categoría" });
+            }
+            _unidadTrabajo.Categoria.Remover(categoriaDb);
+            await _unidadTrabajo.Guardar();
+            return Json(new { success = true, message = "Categoría borrada exitosamente" });
+        }
+
+        [ActionName("ValidarNombre")]
+        public async Task<IActionResult> ValidarNombre(string nombre, int id = 0)
+        {
+            bool valor = false;
+            var lista = await _unidadTrabajo.Categoria.ObtenerTodos();
+            if (id == 0)
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim());
+            }
+            else
+            {
+                valor = lista.Any(b => b.Nombre.ToLower().Trim() == nombre.ToLower().Trim() && b.Id != id);
+            }
+            if (valor)
+            {
+                return Json(new { data = true });
+            }
+            return Json(new { data = false });
+
+        }
+
+        #endregion
+    }
+}
