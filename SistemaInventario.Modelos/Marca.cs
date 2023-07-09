81745cb88284d7de8ca645cc0861c8b8bf6d5c14
GabrielManco <gabomansep@gmail.com>
2023-07-09 09:51:24 -0500

    CRUD Marca

--- SistemaInventario.Modelos/Marca.cs
+++ SistemaInventario.Modelos/Marca.cs
@@ -4,21 +4,20 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Timers;
 
 namespace SistemaInventario.Modelos
 {
-    public class Bodega
+    public class Marca
     {
         [Key]
         public int Id { get; set; }
 
         [Required(ErrorMessage ="El nombre es requerido")]
-        [MaxLength(60, ErrorMessage ="El nombre sólo permite 60 caracteres")]
+        [MaxLength(60, ErrorMessage ="Máximo 60 caracteres para el nombre")]
         public string Nombre { get; set; }
 
         [Required(ErrorMessage ="La descripción es requerida")]
-        [MaxLength(100, ErrorMessage ="La descripción sólo permite 100 caracteres")]
+        [MaxLength(100, ErrorMessage ="Máximo 100 caracteres para la descripción")]
         public string Descripcion { get; set; }
 
         [Required(ErrorMessage ="El estado es requerido")]
